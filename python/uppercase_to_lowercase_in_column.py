7e851e7eaf58cb72f3cf9cfeebe6e57f2f396076
longwangnsw <longwangnsw@gmail.com>
2021-11-23 15:47:34 +1100

    Update uppercase_to_lowercase_in_column.py

--- python/uppercase_to_lowercase_in_column.py
+++ python/uppercase_to_lowercase_in_column.py
@@ -1,12 +1,8 @@
 import pandas as pd
 import os
 
-i = 0
 for filename in os.listdir('.'):
     if filename.endswith(".csv") and filename.startswith("AWB"):
         data = pd.read_csv(filename)
-        all_lower = data['lanid'].str.islower()
-        if (all_lower.all()):
-            print(filename)
-            i = i + 1
-print(i)
+        data['lanid'] = data['lanid'].str.islower()
+        data.to_csv(filename)
