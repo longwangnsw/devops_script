7d4501de1fe3fe63d4a134ef33088252c9c63b89
longwangnsw <longwangnsw@gmail.com>
2021-11-23 15:53:10 +1100

    Update uppercase_to_lowercase_in_column.py

--- python/uppercase_to_lowercase_in_column.py
+++ python/uppercase_to_lowercase_in_column.py
@@ -6,3 +6,11 @@ for filename in os.listdir('.'):
         data = pd.read_csv(filename)
         data['lanid'] = data['lanid'].str.islower()
         data.to_csv(filename)
+
+# remove duplacate
+for filename in os.listdir('.'):
+    if filename.endswith(".csv") and filename.startswith("AWB"):
+        data = pd.read_csv(filename)
+        print(len(data.index))
+        data_rm_dupl = data.groupby(['app', 'lanid']).agg({'login_date': 'first', 'number_login': 'first'})
+        data_rm_dupl.to_csv(filename)
